5516bbddfedf04d3a0125b663ba27837eb1b7f57
Keith McNulty <keith_mcnulty@mckinsey.com>
2020-02-18 19:08:19 +0000

    again

--- js/graph_slider.js
+++ js/graph_slider.js
@@ -193,7 +193,32 @@ d3.json(dataPath, function(error, graph) {
 		.data(['Degree Centrality', 'Betweenness Centrality', 'Eigenvector Centrality'])
 		.enter().append('option')
 		.attr('value', function(d) { return d.split(' ')[0].toLowerCase(); })
-		.text(function(d) { return d; });
+        .text(function(d) { return d; });
+        
+    var legend = d3.select('svg')
+        .append("g")
+        .selectAll("g")
+        .data(color.domain())
+        .enter()
+        .append('g')
+          .attr('class', 'legend')
+          .attr('transform', function(d, i) {
+            var height = legendRectSize;
+            var x = 0;
+            var y = i * height;
+            return 'translate(' + x + ',' + y + ')';
+        });
+    
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .style('stroke', color);
+    
+    legend.append('text')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function(d) { return d; });
 
 });
 
@@ -234,27 +259,3 @@ function searchNodes() {
 	d3.selectAll('.link').transition().duration(5000).style('stroke-opacity', '0.6');
 }
 
-var legend = d3.select('svg')
-    .append("g")
-    .selectAll("g")
-    .data(color.domain())
-    .enter()
-    .append('g')
-      .attr('class', 'legend')
-      .attr('transform', function(d, i) {
-        var height = legendRectSize;
-        var x = 0;
-        var y = i * height;
-        return 'translate(' + x + ',' + y + ')';
-    });
-
-legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style('fill', color)
-    .style('stroke', color);
-
-legend.append('text')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function(d) { return d; });
